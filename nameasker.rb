f1b100822da2e9e080f8d6e2e19a9b25b7af2e18
Ivan Jimenez <torocatala@gmail.com>
2023-06-29 14:44:56 +0000

    Adds nameasker

---
+++ nameasker.rb
@@ -0,0 +1,9 @@
+# Ask the user for their name
+print "What's your name? "
+name = gets.chomp
+print "How old are you? "
+age = gets.chomp
+
+# Greet the user
+puts "Hello, #{name}! Nice to meet you!"
+puts "You're #{age} years old!"
